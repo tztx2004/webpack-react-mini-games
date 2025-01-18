d5d8a0ae997326c5cc6ee42c7d16105edfdddef4
tztx2004 <tztx2004@gmail.com>
2025-01-18 17:52:18 +0900

    add : 반응속도 게임 추가

--- client.jsx
+++ client.jsx
@@ -1,12 +1,16 @@
 const React = require('react');
 const ReactDOM = require('react-dom/client');
-// const WordRelay = require('./WordRelay');
+
+// components
 import WordRelay from './components/WordRelay/WordRelay';
 import NumberBaseball from './components/NumberBaseball/NumberBaseball';
+import ResponseCheckClass from './components/ResponseCheck/ResponseCheckClass';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
   <>
+    <ResponseCheckClass />
+
     <NumberBaseball />
 
     <WordRelay />
